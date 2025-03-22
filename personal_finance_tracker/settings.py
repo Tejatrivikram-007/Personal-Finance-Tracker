5f6fe1d5afeb289db08443a45b5f0a71024093cd
Tejatrivikram-007 <tejatrivikramvangala98@gmail.com>
2025-03-22 15:16:11 +0530

    update setting

--- personal_finance_tracker/settings.py
+++ personal_finance_tracker/settings.py
@@ -46,6 +46,7 @@ AUTHENTICATION_BACKENDS=[
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    (BASE_DIR/ 'static')
+]
+STATIC_ROOT=(BASE_DIR/ 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
